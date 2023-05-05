fc1218aaa7eb5e57470d16fc84b22670ded3a955
Bahar <bahar@regentmarkets.com>
2023-05-05 16:14:25 +0800

    scaffolding a materialapp

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://www.w3schools.com/images/w3schools_green.jpg'),
+          ),
+        ),
       ),
     ),
   );
